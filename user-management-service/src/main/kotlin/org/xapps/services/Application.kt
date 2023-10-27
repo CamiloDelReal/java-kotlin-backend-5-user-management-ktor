02dc7dac90732e0caa07e5b8908c78e91411de3d
Camilo del Real <cjdelreal2009@gmail.com>
2023-10-27 13:27:05 +0200

    EDIT: Cleaning up commented sources

--- user-management-service/src/main/kotlin/org/xapps/services/Application.kt
+++ user-management-service/src/main/kotlin/org/xapps/services/Application.kt
@@ -3,25 +3,6 @@ package org.xapps.services
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
 import org.xapps.services.plugins.*
-import org.xapps.services.services.RoleService
-import org.xapps.services.services.UserService
-import org.xapps.services.services.UserRoleService
-
-//fun main() {
-//    embeddedServer(
-//        Netty
-//    ) {
-//        RoleService.instance.init()
-//        UserService.instance.init()
-//        UserRoleService.instance.init()
-//        RoleService.instance.seed()
-//        UserService.instance.seed()
-//        configureSecurity()
-//        configureRouting()
-//        configureSerialization()
-//    }.start(wait = true)
-//}
-
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
